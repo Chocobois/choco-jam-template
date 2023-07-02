ee7ae0684364e6a91b1397a312a5c6c4c8b9a854
Dan-Eric Næss <dan-eric.naess@live.no>
2023-07-03 01:10:24 +0200

    Add count to build number

--- automation/git-version.ts
+++ automation/git-version.ts
@@ -4,8 +4,10 @@ import { writeFileSync } from 'fs';
 
 const WriteGitVersion = () => {
     const short = execSync('git rev-parse --short HEAD');
+    const count = execSync('git rev-list --count HEAD');
     const out = {
-        version: short.toString().trim()
+        version: short.toString().trim(),
+        revision: count.toString().trim()
     }
     writeFileSync('./src/version.json', JSON.stringify(out));
 }
