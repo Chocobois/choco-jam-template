164fa487ae34dc147325009216cd73cf3177ec18
Dan-Eric Næss <dan-eric.naess@live.no>
2023-07-20 18:38:53 +0200

    Add icon to Windows build

--- automation/win-bundle.ts
+++ automation/win-bundle.ts
@@ -1,22 +1,39 @@
 import { PluginOption } from 'vite';
 import { title, title_dashed, game_dir, build_path } from './util/constants';
-import { mkdirSync, copyFileSync } from 'fs';
+import { mkdirSync, copyFileSync, readFileSync, writeFileSync } from 'fs';
+import { NtExecutable, NtExecutableResource, Data, Resource } from 'resedit';
+import pngToIco from 'png-to-ico';
 
-const BuildMacApp = () => {
+const BuildWinApp = async () => {
 	console.log(`Packaging Windows exe...`);
 
 	const out_dir = `./dist/win/${title_dashed}`;
 
 	mkdirSync('./dist/win');
 	mkdirSync(out_dir);
-	copyFileSync(`${build_path}/${game_dir}-win_x64.exe`, `${out_dir}/${title}.exe`);
 	copyFileSync(`${build_path}/resources.neu`, `${out_dir}/resources.neu`);
+
+	const data = readFileSync(`${build_path}/${game_dir}-win_x64.exe`);
+	const exe = NtExecutable.from(data);
+	const res = NtExecutableResource.from(exe);
+	const icon = await pngToIco('./src/public/icon.png');
+	const iconFile = Data.IconFile.from(icon);
+
+	Resource.IconGroupEntry.replaceIconsForResource(
+		res.entries,
+		101,
+		1003,
+		iconFile.icons.map((item) => item.data)
+	)
+	res.outputResource(exe);
+
+	writeFileSync(`${out_dir}/${title}.exe`, Buffer.from(exe.generate()));
 };
 
 export default function buildWinApp() {
 	return {
 		name: 'build-windows-bundle',
-		closeBundle: BuildMacApp,
+		closeBundle: BuildWinApp,
 		apply: 'build',
 	} as PluginOption;
 }
