d9e6d8dc3b36ada86926d0008542102f75453a67
Golen <golen87@hotmail.com>
2023-12-06 14:17:43 +0100

    Fix antialias settings for 3.70

--- src/game.ts
+++ src/game.ts
@@ -8,6 +8,8 @@ const config: Phaser.Types.Core.GameConfig = {
 	type: Phaser.WEBGL,
 	width: 1920,
 	height: 1080,
+	mipmapFilter: "LINEAR_MIPMAP_LINEAR",
+	roundPixels: false,
 	scale: {
 		mode: Phaser.Scale.FIT,
 	},
