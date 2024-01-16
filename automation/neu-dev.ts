25c4e05ee61b43024f0b6ff9775384ef446e3973
Dan-Eric Næss <dan-eric.naess@live.no>
2024-01-16 23:21:10 +0100

    Update template deps

--- automation/neu-dev.ts
+++ automation/neu-dev.ts
@@ -4,7 +4,7 @@ import WriteNeuConfig from './write-neu-config';
 WriteNeuConfig();
 const vite = exec('vite');
 vite.stdout?.pipe(process.stdout);
-exec('neu run --frontend-lib-dev -- --window-enable-inspector=true').on('close', () => {
+exec('neu run -- --window-enable-inspector=true').on('close', () => {
 	vite.kill();
 	process.exit();
 });
